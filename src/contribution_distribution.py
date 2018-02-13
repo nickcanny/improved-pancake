31c0a6c533076e8b5869c9307af94eec15ed4691
nickcanny <ntcantafio@gmail.com>
2018-02-12 22:56:38 -0500

    almost done
    problems with function multi_donations,
    TypeError: int() argument must be a string, a bytes-like object or a number, not 'DataFrame'.
    
    tried changing it to a str() but no luck.

--- src/contribution_distribution.py
+++ src/contribution_distribution.py
@@ -1,7 +1,6 @@
 import pandas as pd
 import numpy as np
 import os
-import datetime
 
 data_in = pd.read_table(os.curdir + './input/itcont_1.txt', sep='|', header = None)
 
@@ -9,69 +8,57 @@ data_in = pd.read_table(os.curdir + './input/itcont_1.txt', sep='|', header = No
 data_in = data_in[[0,7,10,13,14,15]]
 
 data_in = data_in.rename(columns={0:'CMTE_ID', 7:'NAME',
-                                      10: 'ZIP_CODE', 13: 'TRAN_DT',
+                                      10: 'ZIP_CODE', 13: 'TRANS_DT',
                                       14: 'TRANS_AMT', 15: 'OTHER_ID'})
 
 #making null/empty values zero
 data = data_in.fillna(0)
 
-#converting 0 into
+#converting 0 into string for checking validity, iff cmte_id is empty
 data['CMTE_ID'] = data['CMTE_ID'].astype('str')
 cmte_id_check = data['CMTE_ID'] != '0'
 
-#
+#convert 0 into string for checking validity of name, iff name is empty
 data['NAME'] = data['NAME'].astype('str')
-name_check = data['NAME'] != ''
+name_check = data['NAME'] != '0'
 
 #change the column for ZIP_CODE into string to get length
 data['ZIP_CODE'] = data['ZIP_CODE'].astype('str')
 zip_check = data['ZIP_CODE'].str.len() >= 5
 
-date_check = data['TRAN_DT'] != 0
+date_check = data['TRANS_DT'] != 0
 
 transaction_check = data['TRANS_AMT'] != 0
 
 other_id_check = data['OTHER_ID'] == 0
 
-clean_data = data[other_id_check & cmte_id_check]
+clean_data = data[cmte_id_check & name_check & zip_check & date_check & transaction_check & other_id_check]
 
+#changing the date so only the year is printed, after the date is checked
+clean_data['TRANS_DT'] = pd.to_datetime(clean_data['TRANS_DT'], format ='%m%d%Y')
+clean_data['TRANS_DT'] = clean_data['TRANS_DT'].dt.year
 
-clean_data =
+#To get the most current transactions according to the data
+most_recent_donations = clean_data['TRANS_DT'] == max(clean_data['TRANS_DT'])
 
+current_donations = clean_data[most_recent_donations]
 
+def multi_donation(df):
+    a = []
+    a.append(df['TRANS_AMT'])
+    percent = pd.read_table(os.curdir + './input/percentile.txt', header = None)
+    df['ACCUM_TOTAL'] = sum(a)
+    df['INDI_TRANS'] = df['TRANS_AMT']
+    df['NUM_TRANS'] = len(df)
+    n = np.around((percent/100)*len(a))-1
+    df['PERCENTILE'] = int(a[n])
+    try:
+        return df.loc[:, ['ACCUM_TOTAL', 'NUM_TRANS', 'PERCENTILE', 'INDI_TRANS']]
+    except:
+        pass
 
+most_recent_donations.groupby('ZIP_CODE').apply(multi_donation).reset_index(drop = True).head()
 
+end_data = data['CMTE_ID','ZIP_CODE','TRANS_DT','PERCENTILE','ACCUM_TOTAL','NUM_TRANS']
 
-def data_cleaning(stuff):
-    row_num = 0
-    for x in stuff:
-        if stuff[0:4] = 'Nan': #check for empty inputs for first 5 rows
-            data_correct_input.drop(data_correct_input.index[row_num])
-        else:
-            if len(stuff[2]) < 5:
-                data_correct_input.drop(data_correct_input.index[row_num])
-            else:
-                if stuff[5] =/ 'NaN':
-                    data_correct_input.drop(data_correct_input.index[row_num])
-        data_correct_input.append(stuff)
-
-clean_data = data_input_cleaning
-
-data_ = {} #  cleaned and
-
-def data_out(stuff_out):
-    precentile_in = pd.read_table(os.curdir + '\input\percentile.txt')
-    running_total_contributions = []
-    number_of_contributions = []
-    for x in stuff_out:
-        running_percentile += 1
-
-'''
-final output into repeat_donors.txt column names seperated by '|'
-1 CMTE_ID
-2 ZIP_CODE
-3 4 digit year of the contribution
-4 running percentile (calling the precentile from percentile.txt and giving that amount) round up for .50 and down for .49
-5 total contributions from ZIP_CODE ytd
-6 total number of transations from ZIP_CODE ytd
-'''
+end_data.to_csv(r os.curdir +'./output/repeat_donors.txt', header=None, index=None, mode='a', sep='|')
